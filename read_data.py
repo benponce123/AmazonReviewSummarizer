efb01a5fddcdb06dda57e35b0f6f4c1478944a15
Angelica Toledo <toledoam@uci.edu>
2020-02-16 17:44:33 -0800

    read_data
    Function to read the json files

---
+++ read_data.py
@@ -0,0 +1,21 @@
+import json
+import sys
+import os
+
+def read_json(filename):
+    '''
+    Converts the json file into a list.
+
+    Parameter:
+    path: filename of the json file to read
+
+    Return:
+    data: list of reviews
+    '''
+
+    data = [json.loads(review) for review in open(os.path.join(sys.path[0], filename), 'r')]
+    return data
+
+
+#d = read_json('reviews_Musical_Instruments_5.json')
+#print(d[1])
